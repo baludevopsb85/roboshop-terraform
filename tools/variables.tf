7382a7cd996d576ec12599f628aa9f4f166d84f2
balusena <balusena12@gmail.com>
2025-12-30 14:27:05 +0000

    add files to roboshop-terraform.

--- tools/variables.tf
+++ tools/variables.tf
@@ -1,18 +1,24 @@
+# --------------------------------------------------------------------
 # DNS Hosted Zone Configuration
+# --------------------------------------------------------------------
 # To attach EC2 instances and services to the correct Route53 DNS zone
 
 variable "zone_id" {
   default = "Z09055292Q5WKIF45FE2E"
 }
 
+# -----------------------------------------------------------------------------
 # Base AMI for All EC2 Instances
+# -----------------------------------------------------------------------------
 # Defines the Amazon Machine Image used to launch all tool servers consistently
 
 variable "ami" {
   default = "ami-09c813fb71547fc4f"
 }
 
+# -------------------------------------------------------------------------------------------
 # DevOps Tool Servers Configuration
+# -------------------------------------------------------------------------------------------
 # Controls EC2 instances used for DevOps tooling such as GitHub Actions runners and ELK stack
 
 variable "tools" {
@@ -33,13 +39,16 @@ variable "tools" {
 }
 
 
+# ---------------------------------------------------------------------------------
 # Runtime Authentication Token
+# ---------------------------------------------------------------------------------
 # To securely pass sensitive tokens (e.g., GitHub, Vault, or API tokens) at runtime
 
 variable "token" {}
 
-
+# ------------------------------------------------------------------------------
 # Amazon ECR Repositories for Application Images
+# ------------------------------------------------------------------------------
 # Defines Docker image repositories for Roboshop microservices and CI/CD runners
 
 variable "ecr" {
