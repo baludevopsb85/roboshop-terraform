e9f15e68e3efb320b72a48da8b70be077c02329c
balusena <balusena12@gmail.com>
2025-12-30 14:31:43 +0000

    add files to roboshop-terraform.

--- modules/ec2/local.tf
+++ modules/ec2/local.tf
@@ -1,4 +1,6 @@
+# --------------------------------------------------
 # Local IAM Policy List (Base + Custom Permissions)
+# --------------------------------------------------
 # Combines a mandatory base permission (sts:GetCallerIdentity) with user-defined IAM permissions for EC2 instances,
 # creating a single list of effective actions the instance can perform.
 
