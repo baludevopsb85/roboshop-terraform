4a4778c6785af3843ea78f10ac0bf368970b4daf
balusena <balusena12@gmail.com>
2025-12-30 14:24:59 +0000

    add files to roboshop-terraform.

--- tools/provider.tf
+++ tools/provider.tf
@@ -1,12 +1,15 @@
+# -----------------------------------------------------------------------------------------
 # AWS Provider Configuration
-# Defines AWS as the cloud provider and sets the region where all resources will be created.
+# -----------------------------------------------------------------------------------------
+# Defines AWS as the cloud provider and sets the region where all resources will be created
 
 provider "aws" {
   region = "us-east-1"
 }
 
-
+# -----------------------------------------------------------------------------------------------
 # Remote Terraform State Configuration (S3 Backend)
+# -----------------------------------------------------------------------------------------------
 # Stores Terraform state remotely in S3 so teams can collaborate safely and avoid state conflicts
 
 terraform {
@@ -18,7 +21,9 @@ terraform {
 }
 
 
+# ---------------------------------------------------------------------------------------
 # HashiCorp Vault Provider Configuration
+# ---------------------------------------------------------------------------------------
 # Connects Terraform to Vault to securely fetch secrets instead of hardcoding credentials
 
 provider "vault" {
