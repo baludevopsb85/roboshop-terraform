c7f824c12238196ae8e6299a4250c04393880c8c
balusena <balusena12@gmail.com>
2025-12-30 14:13:56 +0000

    add files to roboshop-terraform.

---
+++ variables.tf
@@ -0,0 +1,17 @@
+# -------------------------------
+# Terraform Input Variables
+# -------------------------------
+# This section defines all input variables required for provisioning
+# AWS infrastructure, EKS clusters, VPCs, databases, and related resources.
+# Users can override these variables per environment.
+
+variable "ami" {}
+variable "databases" {}
+variable "env" {}
+variable "zone_id" {}
+variable "zone_name" {}
+variable "token" {}
+variable "eks" {}
+variable "vpc" {}
+variable "bastion_nodes" {}
+variable "kms_arn_id" {}
